9e7010c767b99b5f41dfe76aaa2507df9d897b98
Iggy the Mad <iggythemad@gmail.com>
2018-10-07 13:22:13 +0100

    Cleanup and maintenance.

--- DbLayer/DbDataContext.cs
+++ DbLayer/DbDataContext.cs
@@ -14,16 +14,9 @@ namespace EntrepreneurCommon.DbLayer
 {
     public class DbDataContext:DbContext
     {
-        public DbDataContext( string nameOrConnectionString ) : base(nameOrConnectionString)
-        {
-        }
-        public DbDataContext( DbConnection existingConnection, bool contextOwnsConnection ) : base(existingConnection, contextOwnsConnection)
-        {
-
-        }
-        protected DbDataContext()
-        {
-        }
+        public DbDataContext( string nameOrConnectionString ) : base(nameOrConnectionString) {}
+        public DbDataContext( DbConnection existingConnection, bool contextOwnsConnection ) : base(existingConnection, contextOwnsConnection) {}
+        protected DbDataContext() {}
 
         // Market Models
         public DbSet<DbMarketHistoryModel> MarketHistory { get; set; }
